40e1176928e3cd92afd790bb6a819e0c6762258c
LABBASSIBassem <49342568+LABBASSIBassem@users.noreply.github.com>
2020-12-15 12:19:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import Image from './profile/Image';
 
 import './App.css';
 const profile = {
-  name: "Lisa Simpson"
+  name: "Lisa Simpson",
+  bio:' bit of a loner and social outcast. Lisa is a vegetarian, a strong environmentalist, a feminist, and a Buddhist.',
+  profession: '2nd grade student at Springfield Elementary School'
+  
 }
 const App = () => (
 
   <div className='App' style={{ backgroundColor: 'black' }}>
-
-
     <Image Lisa='Lisa.jpg' alt='Lisa' />
-    <Profile handelName={() => alert(profile.name)}  >{profile.name}</Profile>
-    <Profile >Intelligent and passionate, Lisa Simpson, at eight years old, is the second child of Homer and Marge, the younger sister of Bart, and the older sister of Maggie. Lisa's high intellect and liberal political stance creates a barrier between her and other children her age; therefore she is a bit of a loner and social outcast. Lisa is a vegetarian, a strong environmentalist, a feminist, and a Buddhist.</Profile>
-    <Profile back='white'>2nd grade student at Springfield Elementary School</Profile>
-
-
-
-
+    <Profile fullName={profile.name} bio={profile.bio} profession={profile.profession} handelName={() => alert(profile.name)}>{Image}</Profile>
   </div>
 );
 
